7e606240903159d7952d6802697888720bf43c37
Ahmed <ahmedrefaee631@gmail.com>
2026-02-24 21:39:06 -0800

    Fix NextAuth App Router setup + TypeScript errors

--- src/types/login.type .ts
+++ src/types/login.type .ts
@@ -1,4 +1,4 @@
-import { loginschema } from "@/schemas/auth.schemas";
+import { loginSchema } from "@/schemas/auth.schemas"; 
 import z from "zod";
 
-export type LoginType = z.infer<typeof loginschema>
+export type LoginType = z.infer<typeof loginSchema>;
